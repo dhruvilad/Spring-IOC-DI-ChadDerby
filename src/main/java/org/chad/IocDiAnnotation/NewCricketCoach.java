0441626e206a0167bf2b309f8108bd3607bd7c12
dhruvilad <dhruvi.lad2010@gmail.com>
2023-03-05 05:06:35 +0530

    Setter Injection with Annotation

--- src/main/java/org/chad/IocDiAnnotation/NewCricketCoach.java
+++ src/main/java/org/chad/IocDiAnnotation/NewCricketCoach.java
@@ -8,8 +8,13 @@ public class NewCricketCoach implements Coach{
 
     private FortuneService fortuneService;
 
+    public NewCricketCoach() {
+        System.out.println("inside constructor of new cricket");
+    }
+
     @Autowired
-    public NewCricketCoach(FortuneService fortuneService) {
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("autoworing using setter in newcricket");
         this.fortuneService = fortuneService;
     }
 
