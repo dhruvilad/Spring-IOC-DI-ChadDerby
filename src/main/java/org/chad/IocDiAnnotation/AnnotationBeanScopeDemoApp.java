8dc59ffb9e9c637fc91ef0fa48d095eb73868a7d
dhruvilad <dhruvi.lad2010@gmail.com>
2023-03-05 05:51:13 +0530

    BeanScope with Annotation

---
+++ src/main/java/org/chad/IocDiAnnotation/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,18 @@
+package org.chad.IocDiAnnotation;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("annotation-applicationContext.xml");
+
+        Coach coach1=context.getBean("newCricketCoach", Coach.class);
+        Coach coach2=context.getBean("newCricketCoach", Coach.class);
+
+        System.out.println(coach1);
+        System.out.println(coach2);
+
+        context.close();
+    }
+}
--- src/main/java/org/chad/IocDiAnnotation/NewCricketCoach.java
+++ src/main/java/org/chad/IocDiAnnotation/NewCricketCoach.java
@@ -2,9 +2,11 @@ package org.chad.IocDiAnnotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class NewCricketCoach implements Coach{
 
     private FortuneService fortuneService;
